22725bbc9f6987080c7fa8aa215a8dc6a5ce0307
Diego Gomez <dgg2010@gmail.com>
2020-05-13 11:07:45 -0700

    Create new questionnaires and linked Questionnaire and User Model

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/questionnaires/create', 'QuestionnaireController@create');
+Route::post('/questionnaires', 'QuestionnaireController@store');
+Route::get('/questionnaires/{questionnaire}', 'QuestionnaireController@show');
